ee9adbc45cac8434a335573541f0aa09f726bd48
dan <dans7ar@gmail.com>
2016-09-18 22:44:38 +0100

    Created initial Frame, set width and height variables

--- Pong/src/Frame.java
+++ Pong/src/Frame.java
@@ -1,8 +1,39 @@
+import java.awt.Color;
+import java.awt.Dimension;
 
-public class Frame
+import javax.swing.JFrame;
+
+public class Frame extends JFrame
 {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private final int WIDTH = 600;
+	private final int HEIGHT = 600;
+	private final String GAME_NAME = "Pong - nullerino";
+	private final Color BACKGROUND_COLOR = Color.WHITE;
+
+	public Frame()
+	{
+		this.setSize(new Dimension(WIDTH, HEIGHT));
+		this.setLocationRelativeTo(null);
+		this.setResizable(false);
+		this.setTitle(GAME_NAME);
+		this.setBackground(BACKGROUND_COLOR);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public void start()
+	{
+		this.setVisible(true);
+	}
+
 	public static void main(String[] args)
 	{
-
+		Frame frame = new Frame();
+		frame.start();
 	}
 }
