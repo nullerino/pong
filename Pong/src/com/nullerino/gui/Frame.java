5ff1a3398e2ce44e5ba4b8f8c8add65191fad812
dan <dans7ar@gmail.com>
2016-09-18 23:34:32 +0100

    Added Paddle, Board, and Pong classes. Also updated to Version 0.0.0 - which is stored in the Frame class

--- Pong/src/com/nullerino/gui/Frame.java
+++ Pong/src/com/nullerino/gui/Frame.java
@@ -5,6 +5,8 @@ import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
+import com.nullerino.game.Pong;
+
 public class Frame extends JFrame
 {
 
@@ -17,20 +19,25 @@ public class Frame extends JFrame
 	private final int HEIGHT = 600;
 	private final String GAME_NAME = "Pong - nullerino";
 	private final Color BACKGROUND_COLOR = Color.WHITE;
+	private final String VERSION = "0.0.0";
+	private Pong game;
 
 	public Frame()
 	{
+		game = new Pong();
 		this.setSize(new Dimension(WIDTH, HEIGHT));
 		this.setLocationRelativeTo(null);
 		this.setResizable(false);
-		this.setTitle(GAME_NAME);
+		this.setTitle(GAME_NAME + " v" + VERSION);
 		this.setBackground(BACKGROUND_COLOR);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.add(game);
 	}
 
 	public void start()
 	{
 		this.setVisible(true);
+		game.run();
 	}
 
 	public static void main(String[] args)
